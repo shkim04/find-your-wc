5546a883e14b4a3f00f39e2effc0fff67915a977
shkim04 <gims86410524@gmail.com>
2023-08-03 11:01:03 +0900

    chore: remove console log

--- src/guards/review.guard.ts
+++ src/guards/review.guard.ts
@@ -9,7 +9,7 @@ export class ReviewAuthGuard implements CanActivate {
   async canActivate(context: GqlExecutionContext): Promise<boolean> {
     const ctx = GqlExecutionContext.create(context);
     const { deleteReviewData } = ctx.getArgs();
-    console.log(ctx.getArgs());
+
     const { id, contributedBy, password } = deleteReviewData;
 
     const reviewInfo = await this.reviewsService.getReview({ id: id });
