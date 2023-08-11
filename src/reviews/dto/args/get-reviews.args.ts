237ce4d3822864549e1d5bf26dccc147364b5bbd
shkim04 <gims86410524@gmail.com>
2023-08-11 23:27:55 +0900

    chore: fix review GetReviewArgs type

--- src/reviews/dto/args/get-reviews.args.ts
+++ src/reviews/dto/args/get-reviews.args.ts
@@ -1,9 +1,9 @@
 import { ArgsType, Field } from '@nestjs/graphql';
-import { IsArray } from 'class-validator';
+import { IsString } from 'class-validator';
 
 @ArgsType()
 export class GetReviewsArgs {
   @Field(() => String)
-  @IsArray()
+  @IsString()
   toiletId: string;
 }
