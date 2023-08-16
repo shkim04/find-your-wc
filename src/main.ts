89d1d572cf2224fb499b11511b33b24ef3215869
shkim04 <gims86410524@gmail.com>
2023-08-16 21:49:53 +0900

    chore: configure port using env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(parseInt(process.env.PORT) || 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+
+  await app.listen(port || 3000);
 }
+
 bootstrap();
